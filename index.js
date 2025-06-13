3b35c66b6a3d63bc9918457f5884e0fe0755134a
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-06-14 00:03:48 +0600

    get sheet data

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const { google } = require("googleapis");
 const app = express();
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send({ message: "Server is running " });
-
+app.get("/", async (req, res) => {
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
+
+  //   create client instance for auth
+  const client = await auth.getClient();
+
+  //   instance of google sheet api
+
+  const googlesheets = google.sheets({
+    version: "v4",
+    auth: client,
+  });
+
+  const spreadsheetId = process.env.SHEET_ID;
+
+  const metadata = await googlesheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+  const getRows = await googlesheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Apply!A:A",
+  });
+  res.send({ message: "Server is running ", data: metadata, rows: getRows });
 });
 
 app.listen(8000, () => {
